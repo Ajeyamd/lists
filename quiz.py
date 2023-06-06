a19d685b0b618e32d5bd346ff65a7da93cad50ba
Ajeya <ajeyamd3@gmail.com>
2023-06-06 23:45:00 +0530

    new

---
+++ quiz.py
@@ -0,0 +1,24 @@
+# a=[1,2,3,4,5,6,7,8,9]
+# print(a[::5])                   #[start : end : step]
+
+
+# a=[1,2,3,4,5,6,7,8,9]
+# a[::2]=10,20,30,40,50,60
+# print(a)
+
+# a=[1,2,3,4,5]
+# print(a[3:0:-1])
+
+# def f(value, values):
+#     v = 1
+#     values[0] = 44
+# t = 3
+# v = [1, 2, 3]
+# f(t, v)
+# print(t, v[0])
+
+
+import random
+fruit=['apple', 'banana', 'papaya', 'cherry']
+random.shuffle(fruit)
+print(fruit)
