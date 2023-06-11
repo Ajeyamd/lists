aeb88269054f9588aa0ed1fa18e4d33363d5fa6a
Ajeya <ajeyamd3@gmail.com>
2023-06-11 15:18:09 +0530

    new

--- project.py
+++ project.py
@@ -1,17 +1,33 @@
-t=0
-days=int(input("Enter the number of days "))  
-for days in range(1,days+1):
-    temp=int(input(f"Enter {days}'s temperature "))
-    t=t+temp
-avg=t/days
-print(f"Average = {avg}")
+# t=0
+# days=int(input("Enter the number of days "))  
+# for days in range(1,days+1):
+#     temp=int(input(f"Enter {days}'s temperature "))
+#     t=t+temp
+# avg=t/days
+# print(f"Average = {avg}")
 
-if temp > avg:
-    print(f"{days}'s temperature is above average")
+# if temp > avg:
+#     print(f"{days}'s temperature is above average")
+
+total=0
+temp=[]
+def temperature(numdays):
+    for days in range(1,int(numdays+1)):
+        n=int(input(f"Enter {days}'s temperature "))
+        temp.append(n)
+    avg=sum(temp)/numdays
+    print(f"average= {avg}")
+    above=0
+    for i in temp:
+        if i > avg:
+            above+=1
+    print(f" {above} day(s) above average")
+temperature(int(input("Enter no of days ")))
+
+
+
     
-    
-above=0
-for days in 
+
 
 
 
